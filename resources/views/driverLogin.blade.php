9e9c04c327f34f43b069a83a5e56ebb001b5a8a0
RejowanHossain <rezwan.hossain@outlook.com>
2020-12-30 19:09:55 +0600

    solved error

--- resources/views/driverLogin.blade.php
+++ resources/views/driverLogin.blade.php
@@ -14,34 +14,19 @@
     </div>
     <div class="calc-display-span primary-display" id="display">
     </div>
-        <!-- const btns = document.getElementById("btn");
-const primaryDisplay = document.getElementById("display");
-const secondaryDisplay = document.getElementById("secondary-display"); -->
   </div>
   <div class="calc-display-hr"></div>
   <div class="calc-btn" id="btn">
     <button class="calc-btn-primary" id="seven">1</button>
     <button class="calc-btn-primary" id="eight">2</button>
     <button class="calc-btn-primary" id="nine">3</button>
-    {{-- <button class="calc-btn-primary " id="divide">/</button> --}}
     <button class="calc-btn-primary" id="four">4</button>
     <button class="calc-btn-primary" id="five">5</button>
-    <!-- const btns = document.getElementById("btn");
-const primaryDisplay = document.getElementById("display");
-const secondaryDisplay = document.getElementById("secondary-display"); -->
     <button class="calc-btn-primary" id="six">6</button>
-    {{-- <button class="calc-btn-primary" id="multiply">*</button> --}}
     <button class="calc-btn-primary" id="one">7</button>
     <button class="calc-btn-primary" id="two">8</button>
     <button class="calc-btn-primary" id="three">9</button>
-    {{-- <button class="calc-btn-primary " id="add">+</button> --}}
-    {{-- <button class="calc-btn-primary" id="decimal">.</button> --}}
     <button class="calc-btn-primary" id="zero">0</button>
-        <!-- const btns = document.getElementById("btn");
-const primaryDisplay = document.getElementById("display");
-const secondaryDisplay = document.getElementById("secondary-display"); -->
-    {{-- <button class="calc-btn-primary" id="equals">=</button>
-    <button class="calc-btn-primary " id="subtract">-</button> --}}
     <div class="submit-pin">
         <button class="calc-btn-clear" id="clear">clear</button>
         <button class="calc-btn-clear" id="clear">Submit</button>
