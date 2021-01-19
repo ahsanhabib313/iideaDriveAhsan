f2f05e8c6533dc64de4c4287fcdba423bae413c7
jeoncsetest <jeoncsetest@gmail.com>
2021-01-19 13:55:43 +0100

    upload menu image

--- app/Http/Controllers/manageMenuController.php
+++ app/Http/Controllers/manageMenuController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 use DB;
 use App\Models\Menu;
 use Illuminate\Http\Request;
+use Image;
+use Log;
 
 class manageMenuController extends Controller
 {
     public function insertmenu(Request $request)
     {
         $request->validate([
-            'item_name' => 'required|unique:menus,item_name',
+            'item_name' => 'required|unique:menus',
             'item_image' => '',
-            'item_category' => 'required|unique:menus,item_name',
-            'item_price' => 'required|unique:menus,item_name',
-            'item_description' => 'required|unique:menus,item_name',
+            'item_category' => 'required',
+            'item_price' => 'required',
+            'item_description' => 'required',
         ]);
         // Menu::insert([
         //     'item_name' => $request->item_name,
@@ -25,22 +27,40 @@ class manageMenuController extends Controller
         // ]); 
 
         $item_name = $request->item_name;
+
+        Log::info($item_name);
+        
         $item_category = $request->item_category;
         $item_price = $request->item_price;
         $item_description = $request->item_description;
         $item_image = $request->file('item_image');
         $image_name = time().'.'.$item_image->extension();
-        $item_image->move(public_path('images').$image_name);
-       
 
         $menus = new Menu();
+
         $menus->item_name = $item_name;
-        $menus->item_image = $item_image;
+    
         $menus->item_category = $item_category;
         $menus->item_price = $item_price;
         $menus->item_description = $item_description;
 
         $menus->save();
+        
+        if ($request->hasFile('item_image')) {
+            $path = public_path() . '/' . config('iideadrive.menu_images');
+            Log::info($path);
+            $filename = 'menu_image_' . $menus->id . '.' . strtolower($request->file('item_image')->getClientOriginalExtension());
+
+            $file_full_path = $path . '/' . $filename;
+
+            $request->file('item_image')->move($path, $filename);
+
+            \Storage::put(config('iideadrive.menu_images') . '/' . $filename, file_get_contents($file_full_path));
+        }
+        $menus->item_image = $file_full_path;
+        $menus->save();
+
+        
          return redirect('manageMenu')->with('success','Category Added successfully!');
     }
 
@@ -48,10 +68,7 @@ class manageMenuController extends Controller
     public function showmenu(Request $request){
         $menu_list = Menu::orderBy('id', 'desc')->get();
         $total_menu = Menu::count();
-        // return view('admin.manageMenu', compact('menu_list', 'total_menu'));
         return view('admin.manageMenu', compact('menu_list', 'total_menu'));
-
-
     }
 }
 
---
+++ config/iideadrive.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | menu image directory
+    |--------------------------------------------------------------------------
+    |
+    | all added menu images will be copied in this public directory
+    |
+    */
+
+    'menu_images' => 'user_content/menu_images',
+
+];
